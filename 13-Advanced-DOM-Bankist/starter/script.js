8a24c01a72b5c51cc4ab863b173a878eb28cb1cb
cthomas4293 <cthomas4293@hotmail.com>
2021-03-11 13:03:13 -0500

    finished lecture

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -32,19 +32,39 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// creating a DOM Element
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent =
-  'We use cookies for improved functionality and analytics.';
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class= btn btn--close-cookie">Got It!</button';
+// // creating a DOM Element
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class= btn btn--close-cookie">Got It!</button';
 
-const header = document.querySelector('.header');
+// const header = document.querySelector('.header');
 
-header.append(message);
+// header.append(message);
 
-// delete elements
-message.addEventListener('click', function () {
-  message.remove();
-});
+// // delete elements
+// message.addEventListener('click', function () {
+//   message.remove();
+// });
+
+// // Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// // console.log(getComputedStyle(message).color);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// // console.log(getComputedStyle(message).height);
+
+// // Access root element (documentElement)
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
