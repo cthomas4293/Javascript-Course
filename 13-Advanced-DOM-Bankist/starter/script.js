d164498d2695c37e4030ac126e7c4dffbd1264b4
Carl Thomas <cthomas4293@hotmail.com>
2021-03-24 20:42:26 -0400

    finished lecture

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -214,6 +214,30 @@ allSections.forEach(function (section) {
 });
 
 /////////////////////////////////////////////////////////////////////////////
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  // Replace src att with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+/////////////////////////////////////////////////////////////////////////////
 // // creating a DOM Element
 // const message = document.createElement('div');
 // message.classList.add('cookie-message');
